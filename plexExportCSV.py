415cab46c9ab5e94a7c7dfee31d6868e73f51c7c
corcoranmam <71404312+corcoranmam@users.noreply.github.com>
2021-03-16 15:15:17 +0000

    change name

--- plexExportCSV.py
+++ plexExportCSV.py
@@ -1,17 +1,23 @@
+#import modules and dependencies
 from plexapi.server import PlexServer
 from plexapi.media import Media
 import csv
 
 
-
+#Your plex credentials
 PLEX_URL = 'http://192.168.0.2:32400'
 PLEX_TOKEN = 'mL5Y3ttWity3xv5gTJCR'
-MOVIE_LIBRARIES = ['Test']
+MOVIE_LIBRARIES = ['Test','4K HDR']
+
+#Create plex server instance
+plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+
+#Get list of movies in MOVIE_LIBRARIES
+movies = []
+movies = plex.library.section('Test').all()
 
 movie_list = []
-movies = []
-plex = PlexServer(PLEX_URL, PLEX_TOKEN)
-movies = plex.library.section('Test').all()
+
 
 
 #print("\n\n", movies)
