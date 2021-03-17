9d0412cc1ae052687fab70ee1f2be1af67a8385e
corcoranmam <71404312+corcoranmam@users.noreply.github.com>
2021-03-17 20:53:31 +0000

    v0.0.2

--- plexExportCSV.py
+++ plexExportCSV.py
@@ -3,25 +3,17 @@ from plexapi.server import PlexServer
 from plexapi.media import Media
 import csv
 
-print("""
-  _____  _           ______                       _    _____  _______      __
- |  __ \| |         |  ____|                     | |  / ____|/ ____\ \    / /
- | |__) | | _____  _| |__  __  ___ __   ___  _ __| |_| |    | (___  \ \  / / 
- |  ___/| |/ _ \ \/ |  __| \ \/ | '_ \ / _ \| '__| __| |     \___ \  \ \/ /  
- | |    | |  __/>  <| |____ >  <| |_) | (_) | |  | |_| |____ ____) |  \  /   
- |_|    |_|\___/_/\_|______/_/\_| .__/ \___/|_|   \__|\_____|_____/    \/    
-                                | |                                          
-                                |_|                                          
-""")
 
 #Your plex credentials
 PLEX_URL = 'http://192.168.0.2:32400'
 PLEX_TOKEN = 'mL5Y3ttWity3xv5gTJCR'
-MOVIE_LIBRARIES = ['Test','Test2'] #Add your movie libraries here, same as they are called in plex
+MOVIE_LIBRARIES = ['Films'] #Add your movie libraries here, same as they are called in plex
 
 #Create plex server instance
 plex = PlexServer(PLEX_URL, PLEX_TOKEN)
 print("\nConnecting to your server...")
+print("\nGenerating movie list from selected libraries,please wait...")
+
 
 #Get list of movies in MOVIE_LIBRARIES
 #ovies = []
@@ -37,45 +29,47 @@ for library_list in movies:
 			"Title": movie.title,
 			"Year": movie.year,
 			"Duration(minutes)":round((movie.duration*0.00001666667)),
-			"Type": movie.type,
 			"Rating": movie.rating,
-			"LastViewed" : movie.lastViewedAt,
 			"Genres": movie.genres,
 			"Studio": movie.studio,
 			"Content Rating": movie.contentRating
 						   })
 
-print("\nGenerating movie list from selected libraries...")
-print("\nThere are a total of ",len(movie_list), "movies in the selected libraries" )
-
 
 #Add the extra information from the media object from each movie
-full_list = []
+media_object_list = []
 for plexlib in movies:
 	for film in plexlib:
-		full_list.append(film.media)
+		media_object_list.append(film.media)
 
 for i in range(len(movie_list)):
-	for media in full_list[i]:
+	for media in media_object_list[i]:
 		movie_list[i].update({
 			"Video Resolution": media.videoResolution,
 			"Video Codec": media.videoCodec,
-			"Video Framerate": media.videoFrameRate,
 			"Video Profile": media.videoProfile,
 			"Container": media.container,
 			"Apsect Ratio": media.aspectRatio,
-			"Height": media.height,
-			"Width": media.width,
 			"Audio Channels": media.audioChannels,
-			"Audio Codec": media.audioCodec,
-			"Audio Profile": media.audioProfile
+			"Audio Codec": media.audioCodec
 			})
 
 #Add the location of the movies from the mediapart object from media object
 
+for i in range(len(movie_list)):
+	for media in media_object_list[i]:
+		for parts in media.parts:
+			movie_list[i].update({
+				"Size (GB)" : round(parts.size/1073741824,2),
+				"LocationOnDisk": part.file
+				})
+		
+				
 
 #Create the labels from they keys of the dictionary of the first movie
 labels = [key for key in movie_list[0]]
+
+print("\nThere are a total of ",len(movie_list), "movies in the selected libraries" )
 
 #Write the dictionary to a csv
 try:
@@ -84,5 +78,6 @@ try:
         writer.writeheader()
         for elem in movie_list:
             writer.writerow(elem)
+    print("Your CSV is ready!...")
 except IOError:
     print("I/O error")
--- test.py
+++ test.py
@@ -7,40 +7,77 @@ import csv
 #Your plex credentials
 PLEX_URL = 'http://192.168.0.2:32400'
 PLEX_TOKEN = 'mL5Y3ttWity3xv5gTJCR'
-MOVIE_LIBRARIES = ['Test','Test2']
+MOVIE_LIBRARIES = ['Films'] #Add your movie libraries here, same as they are called in plex
 
 #Create plex server instance
 plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+print("\nConnecting to your server...")
+print("\nGenerating movie list from selected libraries,please wait...")
+
 
 #Get list of movies in MOVIE_LIBRARIES
 #ovies = []
 movies = [plex.library.section(library).all() for library in MOVIE_LIBRARIES]
 movie_list = []
 
-#print(movies)
+
+#Create the initial list of dictionaries using the Movie object from plexapi
 for library_list in movies:
 	for movie in library_list:
-		movie_list.append({"Title" : movie.title,
-						   "Type" : movie.type,
-						   "LastViewed" : movie.lastViewedAt
+		movie_list.append({
+			"addedAt": movie.addedAt,
+			"Title": movie.title,
+			"Year": movie.year,
+			"Duration(minutes)":round((movie.duration*0.00001666667)),
+			"Rating": movie.rating,
+			"Genres": movie.genres,
+			"Studio": movie.studio,
+			"Content Rating": movie.contentRating
 						   })
 
 
+#Add the extra information from the media object from each movie
+media_object_list = []
+for plexlib in movies:
+	for film in plexlib:
+		media_object_list.append(film.media)
 
-print(movie_list)
-print("\n length of movie list is ", len(movie_list))
+for i in range(len(movie_list)):
+	for media in media_object_list[i]:
+		movie_list[i].update({
+			"Video Resolution": media.videoResolution,
+			"Video Codec": media.videoCodec,
+			"Video Profile": media.videoProfile,
+			"Container": media.container,
+			"Apsect Ratio": media.aspectRatio,
+			"Audio Channels": media.audioChannels,
+			"Audio Codec": media.audioCodec
+			})
 
-for library_list in movies:
-	for movie in library_list:
-		for media in movie.media:
-			for movie in movie_list:
-				movie.update({"Bitrate" : media.bitrate})
+#Add the location of the movies from the mediapart object from media object
 
+for i in range(len(movie_list)):
+	for media in media_object_list[i]:
+		for parts in media.parts:
+			movie_list[i].update({
+				"Size (GB)" : round(parts.size/1073741824,2),
+				"LocationOnDisk": part.file
+				})
 		
+				
 
+#Create the labels from they keys of the dictionary of the first movie
+labels = [key for key in movie_list[0]]
 
+print("\nThere are a total of ",len(movie_list), "movies in the selected libraries" )
 
-
-print(movie_list[0])
-
-
+#Write the dictionary to a csv
+try:
+    with open('movies.csv', 'w') as movies_csv:
+        writer = csv.DictWriter(movies_csv, fieldnames=labels)
+        writer.writeheader()
+        for elem in movie_list:
+            writer.writerow(elem)
+    print("Your CSV is ready!...")
+except IOError:
+    print("I/O error")
